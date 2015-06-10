c350b614185ec8c0e0182013002de2fc0f5f4447
DerNapster <christopher.werning@gmail.com>
2015-06-10 17:44:40 +0200

    categories, vendors funktionieren vollständig

---
+++ app/src/main/java/de/budget/BudgetAndroid/AsyncTasks/LogoutTask.java
@@ -0,0 +1,7 @@
+package de.budget.BudgetAndroid.AsyncTasks;
+
+/**
+ * Created by christopher on 10.06.15.
+ */
+public class LogoutTask {
+}
---
+++ app/src/main/java/de/budget/BudgetAndroid/AsyncTasks/getVendorsTask.java
@@ -0,0 +1,69 @@
+package de.budget.BudgetAndroid.AsyncTasks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import de.budget.BudgetAndroid.BudgetAndroidApplication;
+import de.budget.BudgetAndroid.MainActivity;
+import de.budget.BudgetService.Response.CategoryListResponse;
+
+/*
+    * @author Christopher
+    * @date 09.06.2015
+    */
+public class getVendorsTask extends AsyncTask<String, Integer, CategoryListResponse>
+{
+    private Context context;
+    private static MainActivity activity;
+    private static BudgetAndroidApplication myApp;
+
+    public getVendorsTask(Context context, BudgetAndroidApplication myApp, MainActivity pActivity)
+    {
+        this.context = context;
+        this.activity = pActivity;
+        this.myApp = myApp;
+    }
+
+
+    @Override
+    protected CategoryListResponse doInBackground(String... params){
+        if(params.length != 0)
+            return null;
+
+        try {
+            CategoryListResponse myCategorys = myApp.getBudgetOnlineService().getCategorys(myApp.getSession());
+            Integer rt =  myCategorys.getReturnCode();
+            Log.d("INFO", "Returncode: " + rt.toString());
+            return myCategorys;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected void onProgessUpdate(Integer... values)
+    {
+        //wird in diesem Beispiel nicht verwendet
+    }
+
+    protected void onPostExecute(CategoryListResponse result)
+    {
+        int duration = Toast.LENGTH_SHORT;
+        if(result != null)
+        {
+            //erfolgreich eingeloggt
+            if (result.getReturnCode() == 200){
+
+                myApp.setCategories(result.getCategoryList());
+                Log.d("INFO", "KategorieListe erfolgreich angelegt.");
+            }
+        }
+        else
+        {
+            Log.d("INFO", "Kategorien konnten nicht geladen werden.");
+
+        }
+    }
+}
