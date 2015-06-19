84bd3c465f11979d9b55cf752bc58be36f2a63ea
mark-mit-k <warneke.mark@gmail.com>
2015-06-19 17:14:46 +0200

    Listview fuer Incomes plus refactor

---
+++ app/src/main/java/de/budget/BudgetAndroid/Income/IncomeArrayAdapter.java
@@ -0,0 +1,54 @@
+package de.budget.BudgetAndroid.Income;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import de.budget.BudgetAndroid.Annotations.Author;
+import de.budget.BudgetService.dto.IncomeTO;
+import de.budget.R;
+
+/**
+ * Created by mark on 19/06/15.
+ */
+@Author(name="Mark")
+public class IncomeArrayAdapter extends ArrayAdapter<IncomeTO> {
+
+
+        private final Context context;
+        private final List<IncomeTO> values;
+
+
+        public IncomeArrayAdapter(Context context, int resource, List<IncomeTO> values) {
+            super(context, R.layout.listview_loss, values);
+
+            this.context    = context;
+            this.values     = values;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.listview_income, parent, false);
+
+            SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy");
+
+            TextView    incomeName       = (TextView) rowView.findViewById(R.id.income_name);
+            TextView    incomeDate       = (TextView) rowView.findViewById(R.id.income_date);
+            TextView    incomeNotice     = (TextView) rowView.findViewById(R.id.income_notice);
+            TextView    incomeTotal      = (TextView) rowView.findViewById(R.id.income_total);
+
+            incomeName        .setText(values.get(position).getName());
+            incomeDate        .setText(DATE_FORMAT.format(values.get(position).getReceiptDate()));
+            incomeNotice      .setText(values.get(position).getNotice());
+            incomeTotal       .setText(String.valueOf(values.get(position).getAmount()) + " €");
+
+            return rowView;
+        }
+}
